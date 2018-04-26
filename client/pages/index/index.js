0f0533d30f045bfdbb660ef7a7a001c211ce3008
冀康鑫 <2841253158@qq.com>
2018-04-26 14:27:20 +0800

    叮叮课堂

---
+++ client/pages/index/index.js
@@ -0,0 +1,38 @@
+Page({
+  data: {
+    i: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    i1:"../../images/1.jpg",
+    t1:"电路电路电路电路电路电路电路电路电路电路",
+    mode: 'aspectFit',
+    i2:"../../images/4.png",
+    i3:"../../images/a7.png",
+    is:[
+      {
+        id: 1,
+        image:"../../images/120.jpg"
+      },
+      {
+        id: 2,
+        image: "../../images/135.jpg"
+      },
+      {
+        id: 3,
+        image: "../../images/144.jpg"
+      },
+      {
+        id: 4,
+        image: "../../images/149.jpg"
+      },
+
+
+    ]
+  }
+})
